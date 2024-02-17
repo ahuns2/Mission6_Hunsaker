8d87b8ceb61e06e1ac87884af7f951bfa7071db3
Andrew Hunsaker <ahuns2@byu.edu>
2024-02-16 23:52:31 -0700

    Add project files.

---
+++ Mission6_Hunsaker/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+using Microsoft.AspNetCore.Mvc;
+using Mission6_Hunsaker.Models;
+using System.Diagnostics;
+
+namespace Mission6_Hunsaker.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult AddMovie()
+        {
+            return View();
+        }
+
+        public IActionResult GetToKnowJoel()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Mission6_Hunsaker/Models/AddMovie.cs
@@ -0,0 +1,21 @@
+﻿namespace Mission6_Hunsaker.Models
+{
+    public class AddMovie
+    {
+        public string Category { get; set; }
+
+        public string Title { get; set; }
+
+        public int Year { get; set; }
+
+        public string Director { get; set; }
+
+        public string Rating { get; set; }
+
+        public bool Edited { get; set; }
+
+        public string LentTo { get; set; }
+
+        public string Notes {  get; set; }
+    }
+}
